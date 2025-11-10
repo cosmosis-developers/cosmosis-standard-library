b68f6ed6f5f818dd12574ad7c0ea35ae270e6193
Joe Zuntz <joezuntz@googlemail.com>
2025-11-10 13:07:34 +0000

    Update interpolation scheme for sacc nz change

--- number_density/load_nz_sacc/load_nz_sacc.py
+++ number_density/load_nz_sacc/load_nz_sacc.py
@@ -5,11 +5,11 @@ import scipy.interpolate
 
 def setup(options):
     nz_file = options.get_string(option_section, "nz_file")
-    data_sets = options.get_string(option_section, "data_sets")
     prefix_section = options.get_bool(option_section, "prefix_section", True)
+    data_sets = options.get_string(option_section, "data_sets").split()
     uncertainty_names = options.get_string(option_section, "uncertainty_names", default="").split()
     uncertainty_sections = options.get_string(option_section, "uncertainty_sections", default="nz_uncertainty").split()
-    data_sets = data_sets.split()
+
     if not data_sets:
         raise RuntimeError(
             "Option data_sets empty; please set the option data_sets=name1 name2 etc and I will search the sacc file for those tracers")
@@ -18,14 +18,17 @@ def setup(options):
 
     s = sacc.Sacc.load_fits(nz_file)
 
+    # Check if the requested uncertainties are present
     for uncertainty_name in uncertainty_names:
         if uncertainty_name and (uncertainty_name not in s.tracer_uncertainties):
             raise ValueError(f"Requested tracer uncertainty {uncertainty_name} not found in SACC file {nz_file}, available uncertainties: {s.tracer_uncertainties}")
 
-    if uncertainty_names:
-        tracer_uncertainties = {name: s.tracer_uncertainties[name] for name in uncertainty_names if name in s.tracer_uncertainties}
-    else:
-        tracer_uncertainties = {}
+
+    tracer_uncertainties = {
+        name: s.tracer_uncertainties[name] 
+        for name in uncertainty_names 
+        if name in s.tracer_uncertainties
+    }
 
 
     data = {}
@@ -45,12 +48,24 @@ def setup(options):
         nz = tracer.nz
         data[data_set, index] = (z, nz)
 
+    # Check that the number of n(z) values matches the size of the linear transformation matrix
+    for tracer_uncertainty in tracer_uncertainties.values():
+        if not isinstance(tracer_uncertainty, sacc.tracer_uncertainty.NZLinearUncertainty):
+            continue
+        _, nzs_for_tu = get_nzs_for_uncertainty(data, tracer_uncertainty)
+        nzs_for_tu_cat = np.concatenate(nzs_for_tu)
+        n1 = len(nzs_for_tu_cat)
+        n2 =  tracer_uncertainty.linear_transformation.shape[0]
+        if not n1 == n2:
+            raise ValueError(f"Number of n(z) values for tracer uncertainty {tracer_uncertainty.name} ({n1}) does not match the size of its linear transformation matrix ({tracer_uncertainty.linear_transformation.shape})"  )
+
     output = {
         "tracer_uncertainties": tracer_uncertainties,
         "uncertainty_sections": uncertainty_sections,
         "output_nz": data,
         "prefix_section": prefix_section,
     }
+
     # CosmoSIS is currently expecting all the bins to have the
     # same z grid. Changing this is possible but would require
     # some changes later in the pipeline, so we check here.
@@ -68,6 +83,8 @@ def setup(options):
             if (z is not None) and (not np.allclose(zi, z)):
                 raise ValueError(f"z values different for different bins in {nz_file}")
 
+
+
     return output
 
 
@@ -79,7 +96,7 @@ def execute(block, config):
     uncertainty_sections = config["uncertainty_sections"]
     prefix_section = config["prefix_section"]
 
-
+    # Generate the morphed n(z)s if we have any uncertainties to apply
     for tracer_uncertainty, uncertainty_section in zip(tracer_uncertainties.values(), uncertainty_sections):
         nz_info = update_nzs(block, nz_info, tracer_uncertainty, uncertainty_section)
         
@@ -105,6 +122,16 @@ def execute(block, config):
     return 0
 
 
+def get_nzs_for_uncertainty(nz_info, tracer_uncertainty):
+    nzs = []
+    zs = []
+    for name in tracer_uncertainty.tracer_names:
+        name, index = name.split("_")
+        index = int(index)
+        z, nz = nz_info[name, index]
+        nzs.append(nz)
+        zs.append(z)
+    return zs, nzs
 
 def update_nzs(block, nz_info, tracer_uncertainty, uncertainty_section):
     """
@@ -118,14 +145,7 @@ def update_nzs(block, nz_info, tracer_uncertainty, uncertainty_section):
     mu = tracer_uncertainty.mean
 
     # Get the n(z)s as a list in the order expected in the datablock
-    nzs = []
-    zs = []
-    for name in tracer_uncertainty.tracer_names:
-        name, index = name.split("_")
-        index = int(index)
-        z, nz = nz_info[name, index]
-        nzs.append(nz)
-        zs.append(z)
+    zs, nzs = get_nzs_for_uncertainty(nz_info, tracer_uncertainty)
 
     # The alpha parameters are the normally distributed variables that
     # are converted into either new n(z) additions directly or into
@@ -136,8 +156,11 @@ def update_nzs(block, nz_info, tracer_uncertainty, uncertainty_section):
     # In the linear model case the values that are stored in the uncertainty
     # object are the vectors that apply to the n(z) itself, so we just need to do
     # a linear combination of these.
+    # In this case there is a mean object attached to the uncertainty,
+    # but it should always be zero.
+    nzs_cat = np.concatenate(nzs)
     if mode == "linear_combination":
-        new_nzs = np.concatenate(nzs) + M @ alpha
+        new_nzs = nzs_cat + M @ alpha
         new_nzs = np.split(new_nzs, ntracer)
     else:
         # Otherwise, the values stored are shift (and perhaps stretch) values.
@@ -145,6 +168,12 @@ def update_nzs(block, nz_info, tracer_uncertainty, uncertainty_section):
         if mode == "shift":
             shifts = mu + M @ alpha
             width = 1 + np.zeros_like(shifts)
+
+            # write the shift parameters to the block for record-keeping
+            for i, shift in enumerate(shifts):
+                nm = tracer_uncertainty.tracer_names[i]
+                block[uncertainty_section, f"bias_{nm}"] = shift
+
         elif mode == "shift_stretch":
             # In the shift-stretch case, the parameters are interleaved shift, stretch
             # values.
@@ -207,8 +236,9 @@ def shift_and_width_model(z, nz, shift, width):
     of the fiducial n(z) distribution and the rescaling by the width.
     Finally the distribution is normalized.
     """
-    nz_i = scipy.interpolate.interp1d(z, nz, kind="linear", fill_value="extrapolate")
+    nz_i = scipy.interpolate.interp1d(z, nz, kind="linear", fill_value=0.0, bounds_error=False)
     mu = np.average(z, weights=nz)
-    pdf = nz_i((z - mu + shift) / width + mu)
-    norm = np.sum(pdf)
-    return pdf / norm
+    pdf = nz_i((z - mu - shift) * width + mu)
+    # The normnalization does not matter in cosmosis
+    # as we always renormalize later, but so we do not normalize here.
+    return pdf
