db9f631ff4515d62d5ddf52d97d98d9dceeac777
Joe Zuntz <joezuntz@googlemail.com>
2025-12-03 12:51:25 +0000

    More tweaks

--- reduce-euclid-deltaz.py
+++ reduce-euclid-deltaz.py
@@ -2,40 +2,48 @@ import sacc
 import numpy as np
 import matplotlib.pyplot as plt
 
-infile = "/Users/jzuntz/src/euclid/txpipe/TXPipe/data/euclid-rr2/outputs-2_1/summary_statistics_real.sacc"
-outfile = "reduced_euclid_rr2_2.1_summary_statistics_real.sacc"
-r = 5
+r = 2
+
+def reduce(infile, outfile):
+    s = sacc.Sacc.load_fits(infile)
+
+    for name, tracer in list(s.tracers.items()):
+        print(name, tracer)
+        z = tracer.z
+        nz = tracer.nz
+        # downsample n(z)
+        nred = len(z) // r
+        zred = []
+        nzred = []
+        for i in range(nred):
+            zred.append(np.mean(z[i * r:(i + 1) * r]))
+            nzred.append(np.mean(nz[i * r:(i + 1) * r]))
+        zred = np.array(zred)
+        nzred = np.array(nzred)
+        cut = zred < 4
+        zred = zred[cut]
+        nzred = nzred[cut]
+        new_tracer = sacc.tracers.NZTracer(name, zred, nzred)
+        s.tracers[name] = new_tracer
 
 
-s = sacc.Sacc.load_fits(infile)
+    for name, tracer in list(s.tracers.items()):
+        print(name, tracer)
+        print(tracer.z.shape)
+        plt.plot(tracer.z, tracer.nz, label=name)
+    plt.xlabel("z")
+    plt.ylabel("n(z)")
+    plt.legend()
+    plt.show()
 
-for name, tracer in list(s.tracers.items()):
-    print(name, tracer)
-    z = tracer.z
-    nz = tracer.nz
-    # downsample n(z)
-    nred = len(z) // r
-    zred = []
-    nzred = []
-    for i in range(nred):
-        zred.append(np.mean(z[i * r:(i + 1) * r]))
-        nzred.append(np.mean(nz[i * r:(i + 1) * r]))
-    zred = np.array(zred)
-    nzred = np.array(nzred)
-    cut = zred < 4
-    zred = zred[cut]
-    nzred = nzred[cut]
-    new_tracer = sacc.tracers.NZTracer(name, zred, nzred)
-    s.tracers[name] = new_tracer
+    s.save_fits(outfile, overwrite=True)
 
 
-for name, tracer in list(s.tracers.items()):
-    print(name, tracer)
-    print(tracer.z.shape)
-    plt.plot(tracer.z, tracer.nz, label=name)
-plt.xlabel("z")
-plt.ylabel("n(z)")
-plt.legend()
-plt.show()
+if __name__ == "__main__":
+    infile = "/Users/jzuntz/src/euclid/txpipe/TXPipe/data/euclid-rr2/outputs-2_1-lensmc/summary_statistics_real.sacc"
+    outfile = f"reduced_{r}_euclid_rr2_2.1_summary_statistics_real.sacc"
+    reduce(infile, outfile)
 
-s.save_fits(outfile)
+    infile = "/Users/jzuntz/src/euclid/txpipe/TXPipe/data/euclid-rr2/outputs-2_1-lensmc/summary_statistics_fourier.sacc"
+    outfile = f"reduced_{r}_euclid_rr2_2.1_summary_statistics_fourier.sacc"
+    reduce(infile, outfile)
